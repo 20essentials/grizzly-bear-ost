431d55c28a8acd657860e12abd9de1edcbc3f91b
DominicDev <s0l0estudi4r@gmail.com>
2025-09-20 23:18:16 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GRIZZLY BEAR';
+export const nameSongs = [
+	"MOURNING SOUND", /*1*/
+	"TWO WEEKS", /*2*/
+	"READY, ABLE", /*3*/
+	"GUN-SHY", /*4*/
+	"YET AGAIN", /*5*/
+	"WHILE YOU WAIT FOR OTHERS", /*6*/
+	"SLEEPING UTE", /*7*/
+	"NEIGHBORS", /*8*/
+	"WILL CALLS", /*9*/
+	"FOUR CYPRESSES", /*10*/
+	"LOSING ALL SENSE", /*11*/
+	"LISTEN AND WAIT", /*12*/
+	"KNIFE", /*13*/
+	"SPEAK IN ROUNDS", /*14*/
+	"ADELMA", /*15*/
+	"THE HUNT", /*16*/
+	"A SIMPLE ANSWER", /*17*/
+	"WHAT'S WRONG", /*18*/
+	"HALF GATE", /*19*/
+	"SUN IN YOUR EYES", /*20*/
+]
